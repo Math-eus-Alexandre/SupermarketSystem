3b03ca6818b973f2b5587c2b9340cf4124f14701
Matheus <thetsmimo2@gmail.com>
2025-09-14 22:27:21 -0300

    Criado funções para ler e formatar os produtos na tabela produtos.csv

--- main.cpp
+++ main.cpp
@@ -1,8 +1,68 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
 using namespace std;
+
+void clearScreen() {
+#ifdef _WIN32
+    system("cls");   // Windows
+#else
+    system("clear"); // Linux/Mac
+#endif
+}
+
+struct Produto{
+    string nome;
+    string preco;    
+};
+
+//Função usada para formatar o arquivo produtos em uma struct Produto. Usada pelo readProdutos()
+Produto separaCamposProd(string linha) {
+    Produto prod;
+    string campo;
+    int prodIndex = 0;
+
+    for (int c = 0; c < linha.length(); c++) {
+        char letra = linha[c];
+        if (letra == ',') {
+            if (prodIndex == 0) {
+                prod.nome = campo;
+            } else if (prodIndex == 1) {
+                prod.preco = campo;
+            }
+            campo = "";
+            prodIndex++;
+        } else {
+            campo += letra;
+        }
+    }
+
+    if (prodIndex == 1) {
+        prod.preco = campo;
+    }
+
+    return prod;
+}
+
+vector<Produto> readProdutos(string table){
+        vector<Produto> produtos;
+        string linha;
+        fstream reader(table);
+        while (getline(reader, linha)) {
+        Produto prodFormatado = separaCamposProd(linha);
+        produtos.push_back(prodFormatado);
+        }
+        reader.close();
+        return produtos;
+    }
+
+
 
 void setMenuOption(int &opcaoMenu)
 {
+    // clearScreen();
     cout << "===========================================================\n"
          << "|                                                         |\n"
          << "|                    (1) Cadastro                         |\n"
@@ -16,16 +76,22 @@ void setMenuOption(int &opcaoMenu)
 
 void cadastrarProduto()
 {
-    cout << "Cadastro de produto selecionado";
+    cout << "Cadastro de produto selecionado" << endl;
+
+    // string teste = "produtos.csv";
+    // readProdutos(teste);
 }
 
-void venderProduto()
+void venderProduto(vector<Produto> produtos)
 {
-    cout << "Venda de produto selecionada";
+    cout << "Venda de produto selecionada" << endl;
+    cout << produtos[0].nome << endl;
+    cout << produtos[1].nome << endl;
 }
 
 int main()
 {
+    vector<Produto> produtos = readProdutos("produtos.csv");
     int opcaoMenu = -1;
     bool deveFechar = false;
 
@@ -42,7 +108,7 @@ int main()
         }
         case 2:
         {
-            venderProduto();
+            venderProduto(produtos);
             break;
         }
         case 3:
